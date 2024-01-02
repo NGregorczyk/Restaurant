75b578f2900ceca36a0bcb3838b9f8f197a6beeb
NGregorczyk <natalia.gregorczyk002@gmail.com>
2024-01-02 15:09:14 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Dania.DaniaMiesne;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DaniaMiesne mielony = new DaniaMiesne("Kotlety schabowe", 50.00, " Mięso, Ziemniaki   ","schab wieprzowy");
+    }
+}
